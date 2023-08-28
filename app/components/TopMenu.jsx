57c9bb693b28320cea342aae860a41c775fef3f3
Nasim <nasimcse16@gmail.com>
2023-08-28 13:22:13 +0600

    top menu section

---
+++ app/components/TopMenu.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const TopMenu = () => {
+  return (
+    <div className="max-w-3xl mx-auto my-10 text-center space-y-5 md:flex md:justify-between md:items-center">
+      <h3 className="text-2xl font-bold">NovelNest</h3>
+      <div className="join">
+        <div>
+          <div>
+            <input
+              className="input input-bordered join-item"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        <div className="indicator">
+          <button className="btn join-item">Search</button>
+        </div>
+      </div>
+      <div>
+        <button className="btn me-4">Login</button>
+        <button className="btn">SignUp</button>
+      </div>
+    </div>
+  );
+};
+
+export default TopMenu;
