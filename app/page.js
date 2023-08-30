4f6238ef493e1bb93b6d9a017da438f3fa32dd76
Nasim <nasimcse16@gmail.com>
2023-08-30 17:24:02 +0600

    add 4 more sections

--- app/page.js
+++ app/page.js
@@ -2,13 +2,27 @@ import Image from 'next/image'
 import Navbar from './components/Navbar'
 import TopMenu from './components/TopMenu'
 import Banner from './components/Banner'
+import AboutPage from './components/AboutPage'
+import Footer from './components/Footer'
+import Navbar2 from './components/Navbar2'
+import Featured from './components/Fetured'
+import Insurance from './components/Insurance'
+import LatestBlog from './components/LatestBlog'
+import TopInteresting from './components/TopInterestiong'
 
 export default function Home() {
   return (
-    <main className='max-w-7xl mx-auto'>
+    <main className='bg-gray-200'>
       <TopMenu />
       {/* <Navbar /> */}
+      <Navbar2 />
       <Banner />
+      <TopInteresting />
+      <AboutPage />
+      <Featured />
+      <Insurance />
+      <LatestBlog />
+      <Footer />
     </main>
   )
 }
